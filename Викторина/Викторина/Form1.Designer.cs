eb40a5ae2e8c835d8a6e53ef41ba6dda1c6fe136
Екатерина <65970270+ekaterinapetrova22@users.noreply.github.com>
2020-05-28 20:03:44 +0300

    Add files via upload

---
+++ Викторина/Викторина/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Викторина
+{
+    partial class Nac
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Nac));
+            this.IGG = new System.Windows.Forms.Button();
+            this.EX = new System.Windows.Forms.Button();
+            this.YRR = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IGG
+            // 
+            this.IGG.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IGG.Location = new System.Drawing.Point(802, 403);
+            this.IGG.Name = "IGG";
+            this.IGG.Size = new System.Drawing.Size(145, 84);
+            this.IGG.TabIndex = 0;
+            this.IGG.Text = "Следущий вопрос";
+            this.IGG.UseVisualStyleBackColor = true;
+            this.IGG.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EX
+            // 
+            this.EX.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EX.Location = new System.Drawing.Point(342, 525);
+            this.EX.Name = "EX";
+            this.EX.Size = new System.Drawing.Size(291, 39);
+            this.EX.TabIndex = 2;
+            this.EX.Text = "Выход";
+            this.EX.UseVisualStyleBackColor = true;
+            this.EX.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // YRR
+            // 
+            this.YRR.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YRR.Location = new System.Drawing.Point(12, 403);
+            this.YRR.Name = "YRR";
+            this.YRR.Size = new System.Drawing.Size(145, 84);
+            this.YRR.TabIndex = 3;
+            this.YRR.Text = "Предыдущий вопрос";
+            this.YRR.UseVisualStyleBackColor = true;
+            this.YRR.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(196, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(575, 376);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(310, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 28);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Отгадай породу собаки";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.radioButton1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(310, 451);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ответ 1";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.radioButton2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(446, 451);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ответ 2";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.radioButton3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(579, 451);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(71, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Ответ 3";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(394, 474);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 45);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Завершить тест";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Nac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = global::Викторина.Properties.Resources._1579187093_23_44;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(959, 564);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.YRR);
+            this.Controls.Add(this.EX);
+            this.Controls.Add(this.IGG);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Nac";
+            this.RightToLeftLayout = true;
+            this.Text = "Угадай породу";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Load += new System.EventHandler(this.Nac_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button IGG;
+        private System.Windows.Forms.Button EX;
+        private System.Windows.Forms.Button YRR;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Викторина/Викторина/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Викторина
+{
+    public partial class Nac : Form
+    {
+        int NumberPicture = 0;
+        int[] Answer;
+        public Nac()
+        {
+            InitializeComponent();
+            Answer = new int[16];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NumberPicture++;
+            if (NumberPicture > 15) NumberPicture = 15;
+            show(NumberPicture);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NumberPicture--;
+            if (NumberPicture < 0) NumberPicture = 0;
+            show(NumberPicture);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int correct = 0;
+            if (Answer[0] == 2)
+                correct++;
+            if (Answer[1] == 3)
+                correct++;
+            if (Answer[2] == 3)
+                correct++;
+            if (Answer[3] == 3)
+                correct++;
+            if (Answer[4] == 2)
+                correct++;
+            if (Answer[5] == 3)
+                correct++;
+            if (Answer[6] == 1)
+                correct++;
+            if (Answer[7] == 2)
+                correct++;
+            if (Answer[8] == 2)
+                correct++;
+            if (Answer[9] == 2)
+                correct++;
+            if (Answer[10] == 2)
+                correct++;
+            if (Answer[11] == 3)
+                correct++;
+            if (Answer[12] == 1)
+                correct++;
+            if (Answer[13] == 3)
+                correct++;
+            if (Answer[14] == 1)
+                correct++;
+            string msg = "Вы не справились с тестом";
+            int prcnt = correct * 100 / 15;
+            if (prcnt >= 75) msg = "Вы справились с тестом";
+            MessageBox.Show("Вы ответили на " + prcnt + "% вопросов верно. " + msg);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Answer[NumberPicture] = 1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Answer[NumberPicture] = 2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Answer[NumberPicture] = 3;
+        }
+
+        private void Nac_Load(object sender, EventArgs e)
+        {
+            var bmp = new Bitmap(Викторина.Properties.Resources.тест11);
+            pictureBox1.BackgroundImage = bmp;
+        }
+        public void show(int NumberPicture)
+        {
+            int NumLabel = NumberPicture + 1;
+            if (NumLabel > 15) NumLabel = 15;
+            label1.Text = "Вопрос №" + NumLabel;
+            switch (Answer[NumberPicture])
+            {
+                case 0:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    break;
+                case 1:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    break;
+                case 2:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    break;
+                case 3:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    break;
+                case 4:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    break;
+                case 5:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    break;
+                case 6:
+                    radioButton1.Checked = true;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    break;
+                case 7:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    break;
+                case 8:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    break;
+                case 9:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    break;
+                case 10:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    break;
+                case 11:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    break;
+                case 12:
+                    radioButton1.Checked = true;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    break;
+                case 13:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    break;
+                case 14:
+                    radioButton1.Checked = true;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    break;
+
+            }
+            switch (NumberPicture)
+            {
+                case 0:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест11);
+                    break;
+                case 1:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест12);
+                    break;
+                case 2:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест13);
+                    break;
+                case 3:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест14);
+                    break;
+                case 4:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест15);
+                    break;
+                case 5:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест16);
+                    break;
+                case 6:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест17);
+                    break;
+                case 7:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест18);
+                    break;
+                case 8:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест19);
+                    break;
+                case 9:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест110);
+                    break;
+                case 10:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест111);
+                    break;
+                case 11:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест112);
+                    break;
+                case 12:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест113);
+                    break;
+                case 13:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест114);
+                    break;
+                case 14:
+                    pictureBox1.BackgroundImage = new Bitmap(Викторина.Properties.Resources.тест115);
+                    break;
+            }
+        }
+    }
+}
+
